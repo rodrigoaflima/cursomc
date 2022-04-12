ec46dae07bffc17ed7436ac5c0ff1e26b65b1a4a
Rodrigo Lima <rodrigoaflima@gmail.com>
2022-04-12 15:48:45 -0300

    estado e cidade

---
+++ src/main/java/com/rodrigolima/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigolima.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigolima.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
