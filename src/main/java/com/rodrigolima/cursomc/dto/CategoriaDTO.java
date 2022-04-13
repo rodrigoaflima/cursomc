2ba8f76a3259ac0b6987530e662077d44cc81de3
Rodrigo Lima <rodrigoaflima@gmail.com>
2022-04-13 18:24:30 -0300

    validação com bean validation

--- src/main/java/com/rodrigolima/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rodrigolima/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,9 @@ package com.rodrigolima.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import com.rodrigolima.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -10,6 +13,8 @@ public class CategoriaDTO implements Serializable{
 	
 	private Integer id;
 	
+	@NotEmpty(message = "Preenchimento obregatório")
+	@Size(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/rodrigolima/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigolima/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigolima.cursomc.domain.Categoria;
+import com.rodrigolima.cursomc.dto.CategoriaDTO;
 import com.rodrigolima.cursomc.repositories.CategoriaRepository;
 import com.rodrigolima.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigolima.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 
