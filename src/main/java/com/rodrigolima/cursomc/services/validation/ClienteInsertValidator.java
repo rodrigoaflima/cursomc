3338ea364996b1d584af9eb80359e3bbccea716c
Rodrigo Lima <rodrigoaflima@gmail.com>
2022-04-18 17:23:10 -0300

    validação customizada para email não repetido na criação de cliente

--- src/main/java/com/rodrigolima/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/rodrigolima/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rodrigolima.cursomc.domain.Cliente;
 import com.rodrigolima.cursomc.domain.enums.TipoCliente;
 import com.rodrigolima.cursomc.dto.ClienteNewDTO;
+import com.rodrigolima.cursomc.repositories.ClienteRepository;
 import com.rodrigolima.cursomc.resources.exception.FieldMessage;
 import com.rodrigolima.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -21,13 +29,24 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		List<FieldMessage> list = new ArrayList<>();
 		// inclua os testes aqui, inserindo erros na lista
 		
+		//Valida cpf
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
 		
+		//Valida email
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		//Valida se email já existe no cadastro de cliente
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
@@ -36,4 +55,6 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		return list.isEmpty();
 	}
+	
+	
 }
